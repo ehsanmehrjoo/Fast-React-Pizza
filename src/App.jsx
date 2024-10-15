61479988cf2f61ba8d20e36b8ba75b86bf94b72f
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-15 17:24:58 +0330

    Update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
- 
- 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './Ui/Home'
+ import Menu from './features/menu/Menu'
+import Cart from "./features/cart/Cart"
+import Order from "./features/order/Order"
+ const router = createBrowserRouter([
+
+   {
+     path : "/",
+     element: <Home />
+    },
+    {
+      path : "/menu",
+      element : <Menu />
+        },
+        {
+          path: "/cart",
+          element : <Cart/>
+        },
+        {
+          path: "/order",
+          element : <Order/>
+        }
+  ]
+  )
  function App() {
 
- 
-   return (
-     <div>Hallo vite!</div>
-   )
+  return <RouterProvider router={router}/> 
  }
  
  export default App
