925c9cf0c7a42fbc1c9e027ca396b2b9ad86102d
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-19 22:14:01 +0330

    Update SearchOrder  component

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
  
 
 function SearchOrder() {
     const [query, setQuery] = useState('');
+    const navigate = useNavigate()
  
     function handelSubmit (e){
         e.preventDefault()
+        if(!query) return
+        navigate(`/order/${query}`)
+        setQuery('')
     }
   return (
     <form onSubmit={handelSubmit}>
