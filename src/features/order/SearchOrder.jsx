be0794310968f6b03ae48e80bed108b87b31aebd
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-19 22:08:44 +0330

    Update SearchOrder  component

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -4,13 +4,16 @@ import { useState } from "react"
 
 function SearchOrder() {
     const [query, setQuery] = useState('');
-
+ 
+    function handelSubmit (e){
+        e.preventDefault()
+    }
   return (
-    <div>
+    <form onSubmit={handelSubmit}>
         
      <input type="text" name="order" value={query} placeholder="Search order #"
       onChange={(e) => setQuery(e.target.value)}/>
-    </div>
+    </form>
   )
 }
 
