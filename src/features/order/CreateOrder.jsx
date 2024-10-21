db89d12d1355a79ced502ece23c36a23272d281f
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-21 17:46:04 +0330

    Update CreateOrder  component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -34,8 +33,8 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-console.log(navigation);
-const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -55,6 +54,7 @@ const isSubmitting = navigation.state === "submitting";
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -72,13 +72,15 @@ const isSubmitting = navigation.state === "submitting";
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
-        <input type="hidden" name="cart" value={JSON.stringify(cart)}/> 
-        <button disabled={isSubmitting}>{isSubmitting ? "Placing order":"Order new"}</button>       
-       </div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
+        </div>
       </Form>
     </div>
   );
@@ -86,22 +88,25 @@ const isSubmitting = navigation.state === "submitting";
 
 export default CreateOrder;
 
-export async function action({request}){
-  
+export async function action({ request }) {
   // Form information extraction
-  const formData = await  request.formData();
+  const formData = await request.formData();
   const data = Object.fromEntries(formData);
-
- 
 
   const order = {
     ...data,
-    cart : JSON.parse(data.cart),
-    priority : data.priority  === "on"
-  }
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
 
- const newOrder = await createOrder(order)
- 
-return redirect(`/order/${newOrder.id}`);
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please provide a valid phone number. We need it to contact you.";
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
-
