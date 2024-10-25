ffcbab6adfaae3b175e51af963326620b76ec540
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-25 09:48:38 +0330

    Update Header  tailwind

--- src/Ui/Header.jsx
+++ src/Ui/Header.jsx
@@ -6,7 +6,7 @@ import Username from "../features/users/Username"
 
 function Header() {
   return (
-    <header className=" uppercase bg-yellow-400 text-gray-700 px-4 py-3 border-b border-stone-500"  >
+    <header className=" uppercase bg-yellow-400 text-gray-700 px-4 py-3 border-b border-stone-500 sm:px-6"  >
         <Link className="tracking-widest" to="/">Fast React Pizza Co.</Link>
         <SearchOrder />
        <Username />
