db823d7c755043c62a5e75ab1cd5c1dcc1e7575d
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-28 10:25:01 +0330

    Update CreateOrder  tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -77,7 +77,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting} className='
+          bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full 
+          hover:bg-yellow-300 duration-500 transition-colors focus:outline-none focus:ring 
+          focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400'>
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
@@ -106,7 +109,8 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+  return null
 }
