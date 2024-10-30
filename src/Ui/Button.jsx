e3fc6dfa550e7b914fa3994b49050f894dc8e6a9
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-10-30 18:09:20 +0330

    Update Cart and  Button  tailwind

--- src/Ui/Button.jsx
+++ src/Ui/Button.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
 
-function Button({disabled , children}) {
+import { Link } from 'react-router-dom'
+
+function Button({disabled , children , to}) {
+  const className =
+    'bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 duration-500 transition-colors focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed disabled:bg-stone-600 sm:px-6 sm:py-4';
+
+ 
+
+  if(to) return <Link className={className} to={to}>{children}</Link>
+
   return (
-    <button disabled={disabled} className='
-    bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full 
-    hover:bg-yellow-300 duration-500 transition-colors focus:outline-none focus:ring 
-    focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed disabled:bg-stone-600 sm:px-6 sm:py4' >
+    <button disabled={disabled}   className={className} >
       {children}
     </button>
   )
