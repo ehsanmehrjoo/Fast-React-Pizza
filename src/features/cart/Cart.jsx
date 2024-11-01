d5dc0d7d9ab028af96d2008a94f31d04a913319f
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-11-01 11:32:56 +0330

    Update   Cart and CartItem    tailwind

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -35,7 +35,7 @@ function Cart() {
       <LinkButton  to="/menu" className='text-sm text-blue-500 hover:text-blue-600 hover:underline'>&larr; Back to menu</LinkButton>
 
       <h2 className='font-semibold text-xl mt-7 mb-5'>Your cart, %NAME%</h2>
-      <ul className='divide-y divide-stone-200 border-b mt-3 px-3'>
+      <ul className='divide-y divide-stone-200 border-b mt-3 '>
         {cart.map((item) => (<CartItem item={item} key={item.key}  />))}
       </ul>
       <div className='mt-6 space-x-2'>
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,15 +1,17 @@
+import Button from "../../Ui/Button";
 import {formatCurrency} from "../../utilities/helpers"
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
+    <li className="py-3 flex items-center justify-between ">
       <p>
         {quantity}&times; {name}
       </p>
-      <div>
+      <div className="flex justify-between items-center space-x-6">
         <p>{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
