85caaca7b640a1a11a60d84d9a7dac4fa8921b04
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-11-08 13:17:35 +0330

    Update  CartOverview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -8,14 +8,18 @@ function CartOverview() {
   console.log(totalCartQuantity);
   const totalCartPrice = useSelector(getTotalCartPrice);
   console.log(totalCartPrice);
+
+  if(!totalCartQuantity) return null;
   return (
-    <div className="bg-stone-700 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base flex items-center justify-between">
+    <>
+      <div className="bg-stone-700 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base flex items-center justify-between">
       <p className=" text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
+    </>
   );
 }
 
