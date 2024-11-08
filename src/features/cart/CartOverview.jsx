306ca448eb8d6c3cb4d236353c3447eb72543461
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-11-08 13:13:46 +0330

    Update  CartOverview and cartSlice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utilities/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity)
@@ -11,7 +12,7 @@ function CartOverview() {
     <div className="bg-stone-700 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base flex items-center justify-between">
       <p className=" text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${totalCartPrice}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
